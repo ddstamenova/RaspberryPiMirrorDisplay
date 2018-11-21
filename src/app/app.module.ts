3c17be3c655da10537e30633e317e1a4b5240ef7
Desislava Stamenova <ds1g16@soton.ac.uk>
2018-11-21 12:53:15 +0000

    Initialisation + set up grid view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MirrorComponent } from './mirror/mirror.component';
+import { ControllerComponent } from './controller/controller.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MirrorComponent,
+    ControllerComponent
   ],
   imports: [
     BrowserModule
